2d866bcf9352c8b6ede08217ffe1d067f0742f64
Evgenii Novozhilov <evgenii.novozhilov@jetbrains.com>
2026-01-09 17:40:55 +0200

    Add include() statement support for MODULE.bazel files
    fixes #6882 #7547

--- base/src/com/google/idea/blaze/base/bazel/BuildSystemProvider.java
+++ base/src/com/google/idea/blaze/base/bazel/BuildSystemProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 The Bazel Authors. All rights reserved.
+ * Copyright 2026 The Bazel Authors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,10 +24,12 @@ import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.fileTypes.ExactFileNameMatcher;
 import com.intellij.openapi.fileTypes.ExtensionFileNameMatcher;
 import com.intellij.openapi.fileTypes.FileNameMatcher;
+import com.intellij.openapi.fileTypes.WildcardFileNameMatcher;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import java.io.File;
+
 import javax.annotation.Nullable;
+import java.io.File;
 
 /**
  * Extension points specify the build systems supported by this plugin.<br>
@@ -193,6 +195,8 @@ public interface BuildSystemProvider {
     possibleWorkspaceFileNames().forEach(s -> list.add(new ExactFileNameMatcher(s)));
     possibleFileExtensions().forEach(s -> list.add(new ExtensionFileNameMatcher(s)));
     possibleModuleFileNames().forEach(s -> list.add(new ExactFileNameMatcher(s)));
+    // Add wildcard matcher for included MODULE.bazel fragments (*.MODULE.bazel)
+    list.add(new WildcardFileNameMatcher("*.MODULE.bazel"));
     return list.build();
   }
 
